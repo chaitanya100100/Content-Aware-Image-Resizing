ba47fc974bbaa1299314d49fb293e07c76d412c5
Chaitanya Patel <chaitanya100100@gmail.com>
2017-11-25 23:53:44 +0530

    basic dp and gradient features

---
+++ get_cost_gradient.m
@@ -0,0 +1,13 @@
+function G_im = get_cost_gradient(im)
+    
+    [row,col,n] = size(im);
+    if(n == 3)
+        im = rgb2gray(im);
+    end
+    im = double(im)/255;
+
+    [grad_x,grad_y] = gradient(im);
+    G_im = grad_x + grad_y;
+    G_im = abs(G_im);
+
+end
---
+++ seam_carving.m
@@ -0,0 +1,42 @@
+clear all ;
+
+im = double(imread('Broadway_tower_edit.jpg')) ;
+[M, N, chn] = size(im) ;
+FM = M / 2 ; FN = N - 30 ;
+
+OM = M ; ON = N ;
+
+% traverse until we get desired width
+while N > FN
+    
+    cost = get_cost_gradient(im) ;
+    dp = zeros(M, N) ;
+    from = zeros(M, N) ;
+    dp(1, :) = cost(1, :) ;
+    from(1, :) = 1 : N ;
+    
+    for i = 2 : M
+        for j = 1 : N
+            dp(i, j) = dp(i - 1, j) ;
+            from(i, j) = j ;
+            if j > 1 && dp(i - 1, j - 1) < dp(i, j)
+                dp(i, j) = dp(i - 1, j - 1) ;
+                from(i, j) = j - 1 ;
+            end
+            if j < N && dp(i - 1, j + 1) < dp(i, j)
+                dp(i, j) = dp(i - 1, j + 1) ;
+                from(i, j) = j + 1 ;
+            end
+            dp(i, j) = dp(i, j) + cost(i, j) ;
+        end
+    end
+    
+    [~, idx] = min(dp(M, :)) ;
+    for i = M : -1 : 1
+        im(i, idx : N - 1, :) = im(i, idx + 1 : N, :) ;
+        idx = from(i, idx) ;
+    end
+    im = im(:, 1 : N - 1, :) ;
+    N = N - 1 ;
+    
+end
